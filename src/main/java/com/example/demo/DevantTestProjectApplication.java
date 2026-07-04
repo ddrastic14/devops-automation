a84ad3470a40971534895d1627a08b4f84dae10e
Devant <ddrastic14@gmail.com>
2026-07-04 09:37:24 -0400

    Initial commit

---
+++ src/main/java/com/example/demo/DevantTestProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevantTestProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevantTestProjectApplication.class, args);
+	}
+
+}
